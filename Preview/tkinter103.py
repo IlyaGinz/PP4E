23a9d0d75b9aaa9590186771093960cd3d36f5c5
Ilya Ginzburg <ilya_g@rad.com>
2023-05-08 20:24:08 +0300

    tkinter continue.

--- Preview/tkinter103.py
+++ Preview/tkinter103.py
@@ -2,10 +2,17 @@ from tkinter import *
 from tkinter.messagebox import showinfo
 
 def reply(name):
-    showinfo(title='Reply', message='HellButton pressed!')
+    showinfo(title='Reply', message=f'Hello {name}')
 
-if __name__=='__main__':
-    window= MyGui()
-    window.pack()
-    window.mainloop()
+top = Tk()
+top.title('Echo')
+#top.iconbitmap('py-blue-trans-out.ico')
+
+Label(top, text='Enter your name:').pack(side=TOP)
+ent = Entry(top)
+ent.pack(side=TOP)
+btn = Button(top, text='Submit', command=(lambda: reply(ent.get())))
+btn.pack(side=LEFT)
+
+top.mainloop()
 
