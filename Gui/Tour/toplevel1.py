3548100787cdcdc50fcb46520a22188e30bcc1d7
Ilya Ginzburg <ilya_g@rad.com>
2023-05-15 16:50:53 +0300

    Gui Tour

---
+++ Gui/Tour/toplevel1.py
@@ -0,0 +1,9 @@
+from tkinter import Tk, Button
+
+win1 = Tk()
+win2 = Tk()
+
+Button(win1, text='Spam', command=win1.destroy).pack()
+Button(win2, text='Eggs', command=win2.destroy).pack()
+
+win1.mainloop()
